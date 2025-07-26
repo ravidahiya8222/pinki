f84c2a07cb02f5c13ea9b3c41fb18b905d99ae67
ravidahiya8222 <ravidahiya789@gmail.com>
2025-07-26 13:11:23 +0530

    f

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "rg" {
+    for_each = var.rg_child
+    name = each.value.name
+    location =each.value.location
+}
+resource "azurerm_resource_group" "rg2q" {
+    for_each = var.rg_child
+    name = each.value.name
+    location =each.value.location
+}
+
+asd
+asc
+
+
+ 
